a644fe106e36a9f2d17be47cfab256e1ec6d5857
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2019-08-13 19:24:42 -0500

    Superivisor.py

--- Supervisor.py
+++ Supervisor.py
@@ -1,1 +1,22 @@
+class Supervisor(Persona):
+    """Clase que representa a un Supervisor"""
 
+    def __init__(self, cedula, nombre, apellido, sexo, rol):
+        """Constructor de clase Supervisor"""
+
+        # Invoca al constructor de clase Persona
+        Persona.__init__(self, cedula, nombre, apellido, sexo)
+
+        # Nuevos atributos
+        self.rol = rol
+        self.tareas = ['10','11','12','13']
+
+    def __str__(self):
+        """Devuelve una cadena representativa al Supervisor"""
+        return "%s: %s %s, rol: '%s', sus tareas: %s." % (
+            self.__doc__[26:37], self.nombre, self.apellido, 
+            self.rol, self.consulta_tareas())
+
+    def consulta_tareas(self):
+        """Mostrar las tareas del Supervisor"""
+        return ', '.join(self.tareas)
