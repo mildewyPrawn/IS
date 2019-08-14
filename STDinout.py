4a6db5b131455331ffba3a03275ae7850398c5fe
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2019-08-13 19:30:13 -0500

    3ra clase

--- STDinout.py
+++ STDinout.py
@@ -1,4 +1,14 @@
 import sys
 
+class Animal:
+  def __init__(self, kingdom, phylum, clas, order, family, genus, specie):
+    self.kingdom = kingdom
+    self.phylum = phylum
+    self.clas = clas
+    self.order = order
+    self.family = family
+    self.genus = genus
+    self.specie = specie
+
 for line in sys.stdin:
     print(line)
