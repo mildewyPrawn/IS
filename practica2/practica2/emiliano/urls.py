ed1c2bb420efcc4770adf95fcd24e3b923bd01ae
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2019-09-09 22:43:38 -0500

    ya caché qué onda

--- practica2/practica2/emiliano/urls.py
+++ practica2/practica2/emiliano/urls.py
@@ -3,6 +3,8 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', TemplateView.as_view(template_name='about')),
+    # path('', views.index, name='index'),
+    path('', views.Index.as_view(), name='index'),
+    # path('about/', TemplateView.as_view(template_name='about')),
+    path('about/', views.About.as_view(), name='about'),
 ]
--- practica2/practica2/emiliano/views.py
+++ practica2/practica2/emiliano/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views import View
+# from django.views.generic import TemplateView
 
 # Create your views here.
 def index(request):
-    print(request.method)
+    # print(request.method)
     template = 'emiliano/index.html'
     context = {}
     return render(request, template, context)
 
 # Estaba siguiendo como el tuturial y no viene esto haha ¿Para qué es?
 # Es que estaba en el tuyo, pero si lo quito no importa (?)
-class Index(TemplateView):
+class Index(View):
     template = 'emiliano/index.html'
-    context = {'title': 'Index'}
+    context = {'title': 'Titulo'}
 
     def get(self, request):
-        print('With class-based views')
+        # print('With class-based views')
+        # self.context["posts"] = posts
         return render(request, self.template, self.context)
 
-class AboutView(TemplateView):
-    template_name = "about.html"
+class About(View):
+    template_name = "emiliano/about.html"
     context = {'title': 'About me'}
 
     def get(self, request):
